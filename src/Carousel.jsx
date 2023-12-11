354b9174566f1a7452e9a6dcd3ec3ae7d5912e25
Brandon-Ritchie <ritchietrumpet@gmail.com>
2023-12-11 17:52:02 -0500

    how to do state in a class component

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -9,6 +9,14 @@ class Carousel extends Component {
     images: ["https://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  // arrow functions don't create new scope
+  handleIndexClick = (e) => {
+    // `this` is now the Carousel component, which is different than Angular
+    this.setState({
+      active: +e.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -18,7 +26,11 @@ class Carousel extends Component {
         <img src={images[active]} alt="animal hero" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // We aren't ever actually pushing this to production and it is a tutorial :shrug:
+            // eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
+              data-index={index}
               key={photo}
               src={photo}
               className={index === active ? "active" : ""}
